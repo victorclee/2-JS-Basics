99006d6474f8164b58d4718917e47cdcb4df9a93
victorclee <victorchristopherlee@gmail.com>
2017-08-13 21:07:33 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,53 @@
+// lecture: variables
+/*
+var name='John';
+console.log(name);
+
+var lastName = 'Smith';
+console.log(lastName);
+
+var age = 26;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+*/
+
+// lecture: variable 2
+
+var name = 'John';
+var age = 26;
+
+// console.log(name + age);
+// console.log(age + age);
+
+var job, isMarried;
+
+// console.log(job);
+
+job = 'teacher';
+isMarried = false;
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried+ '.');
+
+
+
+age = 'thirty six';
+job = 'driver';
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried+ '.');
+
+
+// var lastName = prompt('What is the last name?');
+// console.log(lastName);
+
+alert(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried+ '.')
+;
+
+
+
+
+
+
+
+
