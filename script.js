ca802c09312929c327786bcc520a3afd2652ab24
victorclee <victorchristopherlee@gmail.com>
2017-08-23 20:31:30 -0500

    if/else 1

--- script.js
+++ script.js
@@ -44,31 +44,54 @@ console.log(fullAge);
 // alert(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried+ '.');
 
 // lecture: operators
-var now = 2017;
-var birthYear = now - 26;
+// var now = 2017;
+// var birthYear = now - 26;
 
-birthYear = now - 26 * 2
+// birthYear = now - 26 * 2
 // 2017 - 52
 // 1965
 
-console.log(birthYear);
+// console.log(birthYear);
 
-var ageJohn = 30;
-var ageMark = 30;
+// var ageJohn = 30;
+// var ageMark = 30;
 
-ageJohn = ageMark = (3 + 5) * 4 - 6;
+// ageJohn = ageMark = (3 + 5) * 4 - 6;
 // ageJohn = ageMark = 26
 // ageJohn = 26
-ageJohn++;
-ageMark *= 2;
-console.log(ageJohn);
-console.log(ageMark);
+// ageJohn++;
+// ageMark *= 2;
+// console.log(ageJohn);
+// console.log(ageMark);
 
+///////////////////////////////////////////
+// lecture: if/else statements
 
+// var name = 'John';
+// var age = 26;
+// var isMarried = 'yes';
 
-
-
-
+// if (isMarried === 'yes') {
+//   console.log(name + ' is married!');
+// } else {
+//   console.log(name + ' will hopefully marry soon :)');
+// }
+/*
+isMarried = false;
+if(isMarried) {
+  console.log('YES!');
+} else {
+  console.log('NO');
+}
+*/
+// isMarried = false;
+// if(isMarried) {
+//   console.log('YES!');
+// }
+// double equal in JavaScript does type coercion
+// if(23 == '23') {
+//   console.log('something for you to print');
+// }
 
 
 
