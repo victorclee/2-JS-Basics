dbd6fcf288608b24ffd35c4285430c2a2b1b0319
victorclee <victorchristopherlee@gmail.com>
2017-08-23 20:09:47 -0500

    operators

---
+++ readme.md
@@ -0,0 +1,1 @@
+An intro to JavaScript basics!
--- script.js
+++ script.js
@@ -15,34 +15,70 @@ console.log(fullAge);
 
 // lecture: variable 2
 
-var name = 'John';
-var age = 26;
+// var name = 'John';
+// var age = 26;
 
 // console.log(name + age);
 // console.log(age + age);
 
-var job, isMarried;
+// var job, isMarried;
 
 // console.log(job);
 
-job = 'teacher';
-isMarried = false;
+// job = 'teacher';
+// isMarried = false;
 
-console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried+ '.');
+// console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried+ '.');
 
 
 
-age = 'thirty six';
-job = 'driver';
+// age = 'thirty six';
+// job = 'driver';
 
-console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried+ '.');
+// console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried+ '.');
 
 
 // var lastName = prompt('What is the last name?');
 // console.log(lastName);
 
-alert(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried+ '.')
-;
+// alert(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried+ '.');
+
+// lecture: operators
+var now = 2017;
+var birthYear = now - 26;
+
+birthYear = now - 26 * 2
+// 2017 - 52
+// 1965
+
+console.log(birthYear);
+
+var ageJohn = 30;
+var ageMark = 30;
+
+ageJohn = ageMark = (3 + 5) * 4 - 6;
+// ageJohn = ageMark = 26
+// ageJohn = 26
+ageJohn++;
+ageMark *= 2;
+console.log(ageJohn);
+console.log(ageMark);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
