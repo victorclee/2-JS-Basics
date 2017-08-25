0cd7b04ceaacbd21550b5371743c374bb6f4cdc6
victorclee <victorchristopherlee@gmail.com>
2017-08-24 20:03:25 -0500

    boolean logic and switch

--- script.js
+++ script.js
@@ -94,6 +94,66 @@ if(isMarried) {
 // }
 
 
+/////////////////////////////////
+// lecture: boolean logic and switch
+
+/*
+var age = 20;
+if (age < 20) {
+  console.log("John is a teenager");
+} else if (age >= 20 && age  < 30) {
+  console.log('John is a young man');
+} else {
+  console.log('John is a man');
+}
+*/
+/*
+var job = 'teacher';
+
+job = prompt('What does John do?');
+
+switch (job) {
+  case 'teacher':
+    console.log('John teaches kids');
+    break;
+  case 'driver':
+    console.log('John drives a cab in Chicago')
+    break;
+  case 'cop':
+    console.log('John fights crime');
+    break;
+  default:
+    console.log('John is a Joe');
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
